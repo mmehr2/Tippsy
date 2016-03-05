342ed231cd538e3c351c7d2d5a729cfaf4b8ee1b
Mike Mehr <mmehr2x-dev@yahoo.com>
2016-03-04 19:41:39 -0800

    Add keyboard guide (invisible view) to shadow the size of the keyboard as it comes and goes, and added a constraint outlet on this to control the height of the view from code tracking UIKeyboardWillShow/Hide notifications.

--- Tippsy/ViewController.swift
+++ Tippsy/ViewController.swift
@@ -14,10 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipField: UILabel!
     @IBOutlet weak var totalField: UILabel!
     @IBOutlet weak var tipRateControl: UISegmentedControl!
+    @IBOutlet weak var bottomLayoutConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // this layout constraint controls an invisible view that allows space for the keyboard
+        bottomLayoutConstraint.constant = 0 // testing!
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        addKeyboardObserver()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeKeyboardObserver()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,3 +42,30 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController {
+    // functions to move view around using keyboad notifications when shown or hidden
+    // refer to Stack Overflow here: https://youtu.be/1rktrqZIQ8I?t=1m47s
+    
+    func addKeyboardObserver() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func removeKeyboardObserver() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if let userInfo = notification.userInfo,
+            keyboardSize = (userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue) {
+                bottomLayoutConstraint.constant = keyboardSize.CGRectValue().height
+                view.setNeedsLayout()
+        }
+    }
+    
+    func keyboardWillHide(_: NSNotification) {
+        bottomLayoutConstraint.constant = 0
+        view.setNeedsLayout()
+    }
+}
